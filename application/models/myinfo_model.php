b349990471a60430d1edcb7f65922cc026c85953
shanbhagnarendra1986 <shanbhag.narendra@gmail.comm>
2012-12-25 19:53:48 +0530

    Update application/models/myinfo_model.php

--- application/models/myinfo_model.php
+++ application/models/myinfo_model.php
@@ -21,7 +21,7 @@ Class Myinfo_model extends CI_Model
 		}
 
 	}
-	function employee_personalInfo($userId){
+	function user_personalInfo($userId){
 		$this -> db -> select('weight,height');
 		$this -> db -> from('personal_detail');
 		$this -> db -> where('uid = '."'".$userId."'"); 
@@ -64,17 +64,46 @@ Class Myinfo_model extends CI_Model
 	     $weight        =   $updateData['weight'];
 		 $height        =   $updateData['height'];
         //$date = $_POST['fldYear'] . '-' . $_POST['fldMonth'] . '-' . $_POST['fldDay'];
-		print_r($updateData);
+		
 		$dob = $year. '-' .$month. '-' .$date;
-		echo $update_query1="UPDATE users set dob='".$dob."' where uid='".$uid."'";
+		 $update_query1="UPDATE users set dob='".$dob."' where uid='".$uid."'";
 		$query1 = $this->db->query($update_query1);
 		
-		echo $update_query2="UPDATE personal_detail set weight='".$weight."',height='".$height."'  where uid='".$uid."'";
+	    $update_query2="UPDATE personal_detail set weight='".$weight."',height='".$height."'  where uid='".$uid."'";
 		$query2 =$this->db->query($update_query2);
 		
 		return $query1." ".$query2;
 		
 	}
+    function updateLifeHistory($updateData){
+     $uid   =  $updateData['uid'];
+	 $exercise=$updateData['exercise'];
+	 $alcohol=$updateData['alcohol'];
+	 $smoking=$updateData['smoking'];
+	 $lif_entry_date=date('Y-m-d');
+	   $update_query1="UPDATE life_history set lif_entry_date='".$lif_entry_date."',smoking='".$smoking."',alcohol='".$alcohol."',exercise='".$exercise."'  where uid='".$uid."'";
+		$query1 = $this->db->query($update_query1);
+	}	
+	
+	
+		function user_lifeHistory($userId){
+		$this -> db -> select('lif_entry_date,smoking,alcohol,exercise');
+		$this -> db -> from('life_history');
+		$this -> db -> where('uid = '."'".$userId."'"); 
+		$this -> db -> limit(1);
+		$query = $this -> db -> get();
+
+        if($query -> num_rows() == 1)
+		{
+			return $query->result();
+		}
+		else
+		{
+			return false;
+		}
+   
+   }
+
 	
 	
 }
